24768d8924467e8815a65a8ed1ef92aafe684158
Andy.Hsieh <andy.hsieh@mediatek.com>
2023-02-13 16:26:07 +0800

    dt-bindings: media: mtk-camera: Add mtk-camera document
    Add a DT binding documentation for MediaTek V4L2 camera.
    
    Change-Id: I26384c35212d811b8023f404ab36b3005bfc38e1
    Signed-off-by: Andy.Hsieh <andy.hsieh@mediatek.com>

---
+++ include/dt-bindings/media/mtk-camera.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2023 MediaTek Inc.
+ */
+
+#ifndef __DT_BINDINGS_MTK_CAMERA_H__
+#define __DT_BINDINGS_MTK_CAMERA_H__
+
+/*
+ * Used for the dts property mediatek,stream-enable.
+ * e.g. mediatek,stream-enable = <MTK_CAMERA_PREVIEW>;
+ */
+#define MTK_CAMERA_PREVIEW	1
+#define MTK_CAMERA_VIDEO	2
+#define MTK_CAMERA_CAPTURE	3
+
+#endif /* __DT_BINDINGS_MTK_CAMERA_H__ */
